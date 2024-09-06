6277d2eccf1de49e834d0ee69ea609447e9da1e1
KellyAgi <kenjungtarlit28@gmail.com>
2024-09-06 08:49:55 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+//Anna
+//September 06. 2024
+//First Programming Session
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Anna\t\Rhodora\n\Quitaleg");//Instructor's Naame
+        System.out.println("Kent Alfred T.Tangonan\"s ");//Studemt Name
+        System.out.println("I Love Java Programming");//Comment
+    } 
+}
