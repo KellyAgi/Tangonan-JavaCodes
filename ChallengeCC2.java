02f592717dc32b94203f93658d95fe6160edcbd4
KellyAgi <kenjungtarlit28@gmail.com>
2024-09-23 08:42:17 +0800

    Add files via upload

---
+++ ChallengeCC2.java
@@ -0,0 +1,45 @@
+// Title: Challenge in CC2 
+// Author: Tangonan, Kent Alfred T.
+// Date: September 22, 2024
+// Time: 23:21
+// Location: Philippines
+
+import java.util.Scanner;
+
+public class ChallengeCC2 {
+
+    public static void main(String[] args) {
+        Scanner labChallenge = new Scanner(System.in);
+        int fNum;
+        int sNum;
+        
+        System.out.print("Enter the First Number: ");
+        fNum = labChallenge.nextInt();
+        
+        System.out.print("Enter the Second Number: ");
+        sNum = labChallenge.nextInt();
+        
+        char dot;
+        
+        dot = '.';
+        
+        int sum;
+        int difference;
+        int product;
+        int quotient;
+        int remainder;
+        
+        sum = fNum + sNum;
+        difference = fNum - sNum;
+        product = fNum * sNum;
+        quotient = fNum / sNum;
+        remainder = fNum % sNum;
+        
+        System.out.println("The sum of " + fNum + " and " + sNum + " is " + sum + dot);
+        System.out.println("The difference of " + fNum + " and " + sNum + " is " + difference + dot);
+        System.out.println("The product of " + fNum + " and " + sNum + " is " + product + dot);
+        System.out.println("The quotient of " + fNum + " and " + sNum + " is " + quotient + dot);
+        System.out.println("The remainder of " + fNum + " and " + sNum + " is " + remainder + dot);
+    }
+    
+}
