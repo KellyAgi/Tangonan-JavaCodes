3bea3b5c935833018d1f9524e9ba0aa38319d1a2
KellyAgi <kenjungtarlit28@gmail.com>
2024-09-23 19:48:59 +0800

    Update Cc2Laboratory3.java

--- Cc2Laboratory3.java
+++ Cc2Laboratory3.java
@@ -20,27 +20,27 @@ public class Cc2Laboratory3 {
         int quantity3;
         
         // Get the price of item 1.
-        System.out.print("Enter the price of item 1: ");
+        System.out.print("Enter the price of item 1    : ");
         fItem = groceryShopping.nextInt();
         
         // Get the quantity of the item 1.
-        System.out.print("Enter the quantity of item 1: ");
+        System.out.print("Enter the quantity of item 1 : ");
         quantity1 = groceryShopping.nextInt();
         
         // Get the price of item 2.
-        System.out.print("Enter the price of item 2: ");
+        System.out.print("Enter the price of item 2    : ");
         sItem = groceryShopping.nextInt();
         
         // Get the quantity of the item 2.
-        System.out.print("Enter the quantity of item 2: ");
+        System.out.print("Enter the quantity of item 2 : ");
         quantity2 = groceryShopping.nextInt();
         
         // Get the price of item 3.
-        System.out.print("Enter the price of item 3: ");
+        System.out.print("Enter the price of item 3    : ");
         tItem = groceryShopping.nextInt();
         
         // Get the quantity of the item 3.
-        System.out.print("Enter the quantity of item 3: ");
+        System.out.print("Enter the quantity of item 3 : ");
         quantity3 = groceryShopping.nextInt();
         
         // Declaration
@@ -52,7 +52,7 @@ public class Cc2Laboratory3 {
         double taxSales;
         
         // 5% discount
-        percentage = 0.05d;
+        percentage = 0.05;
         // 12% discount
         taxSales = 0.12d;
         
@@ -62,11 +62,10 @@ public class Cc2Laboratory3 {
         salesTax = taxSales*(subtotal-discount);
         finalTotal = (subtotal-discount) + salesTax;
         
-        // Display the total.
-        System.out.printf("\nSubtotal: PHP %.2f", subtotal);
-        System.out.printf("\nDiscount: PHP %.2f", discount);
-        System.out.printf("\nSales Tax: PHP %.2f",  salesTax);
-        System.out.printf("\nFinal Total: PHP %.2f\n", finalTotal);
+        System.out.printf("Subtotal    : PHP %.2f", subtotal);
+        System.out.printf("\nDiscount    : PHP %.2f", discount);
+        System.out.printf("\nSales Tax   : PHP %.2f",  salesTax);
+        System.out.printf("\nFinal Total : PHP %.2f\n", finalTotal);
         
         // Close the scanner
         groceryShopping.close();
