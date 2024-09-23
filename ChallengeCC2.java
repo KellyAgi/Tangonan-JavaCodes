84c22ff810908b68ddc67dfb474000912eba24c8
KellyAgi <kenjungtarlit28@gmail.com>
2024-09-23 09:47:23 +0800

    Update ChallengeCC2.java

--- ChallengeCC2.java
+++ ChallengeCC2.java
@@ -10,31 +10,38 @@ public class ChallengeCC2 {
 
     public static void main(String[] args) {
         Scanner labChallenge = new Scanner(System.in);
+        // Declaration
         int fNum;
         int sNum;
-        
+
+        // Get the first number.
         System.out.print("Enter the First Number: ");
         fNum = labChallenge.nextInt();
-        
+
+        // Get the second number.
         System.out.print("Enter the Second Number: ");
         sNum = labChallenge.nextInt();
-        
+
+        // Declaration
         char dot;
-        
+        // Assignment
         dot = '.';
-        
+
+        // Declaration
         int sum;
         int difference;
         int product;
         int quotient;
         int remainder;
-        
+
+        // Formula 
         sum = fNum + sNum;
         difference = fNum - sNum;
         product = fNum * sNum;
         quotient = fNum / sNum;
         remainder = fNum % sNum;
-        
+
+        // Print and display the output.
         System.out.println("The sum of " + fNum + " and " + sNum + " is " + sum + dot);
         System.out.println("The difference of " + fNum + " and " + sNum + " is " + difference + dot);
         System.out.println("The product of " + fNum + " and " + sNum + " is " + product + dot);
